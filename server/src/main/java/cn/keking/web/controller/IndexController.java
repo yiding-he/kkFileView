82cb54228f868e61967fe80458ca87f1c469c124
Yiding He <yiding.he@gmail.com>
2024-01-10 15:19:49 +0800

    允许通过 spring profile 关闭主页

--- server/src/main/java/cn/keking/web/controller/IndexController.java
+++ server/src/main/java/cn/keking/web/controller/IndexController.java
@@ -1,40 +1,43 @@
 package cn.keking.web.controller;
 
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 /**
- *  页面跳转
+ * 页面跳转
+ *
  * @author yudian-it
- * @date 2017/12/27
+ * @since 2017/12/27
  */
 @Controller
+@Profile("!index-disabled")
 public class IndexController {
 
-    @GetMapping( "/index")
-    public String go2Index(){
-        return "/main/index";
-    }
+  @GetMapping("/index")
+  public String go2Index() {
+    return "/main/index";
+  }
 
-    @GetMapping( "/record")
-    public String go2Record(){
-        return "/main/record";
-    }
+  @GetMapping("/record")
+  public String go2Record() {
+    return "/main/record";
+  }
 
-    @GetMapping( "/sponsor")
-    public String go2Sponsor(){
-        return "/main/sponsor";
-    }
+  @GetMapping("/sponsor")
+  public String go2Sponsor() {
+    return "/main/sponsor";
+  }
 
-    @GetMapping( "/integrated")
-    public String go2Integrated(){
-        return "/main/integrated";
-    }
+  @GetMapping("/integrated")
+  public String go2Integrated() {
+    return "/main/integrated";
+  }
 
-    @GetMapping( "/")
-    public String root() {
-        return "/main/index";
-    }
+  @GetMapping("/")
+  public String root() {
+    return "/main/index";
+  }
 
 
 }
